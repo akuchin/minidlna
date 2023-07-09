3ad34fb7c1833fe464d37f40ec00d0be8e50883d
Artem Kuchyn <aakuchin@gmail.com>
2023-07-09 21:50:21 +0300

    fix scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-if [ -e /run/minidlna/minidlna.pid ]; then
-    rm /run/minidlna/minidlna.pid
-fi
+export PIDFILE='/minidlna/minidlna.pid'
+export PUID="${PUID:-100}"
+export PGID="${PGID:-101}"
 
-exec /usr/sbin/minidlnad -S
+echo '=== Set user and group identifier'
+groupmod --non-unique --gid "$PGID" minidlna
+usermod --non-unique --uid "$PUID" minidlna
+
+echo '=== Set permissions'
+mkdir -p /minidlna/
+chown -R "${PUID}:${PGID}" /minidlna/
+
+
+exec su-exec minidlna /usr/sbin/minidlnad -P "$PIDFILE" -S "$@"
